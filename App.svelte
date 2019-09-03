b170a115df7eb89744fa317246122ce6f96172a4
Hunter Perrin <hperrin@gmail.com>
2019-09-03 14:36:20 -0700

    Initial commit.

---
+++ App.svelte
@@ -0,0 +1,25 @@
+<div class="container">
+  <Button on:click={() => clicked++}>
+    <Icon class="material-icons">thumb_up</Icon>
+    <Label>Click Me</Label>
+  </Button>
+  <p class="mdc-typography--body1">
+    {#if clicked}
+      You've clicked the button {clicked} time{clicked === 1 ? '' : 's'}.
+    {:else}
+      <span class="grayed">You haven't clicked the button.</span>
+    {/if}
+  </p>
+</div>
+
+<script>
+  import Button, {Label, Icon} from '@smui/button';
+
+  let clicked = 0;
+</script>
+
+<style>
+  .grayed {
+    opacity: .6;
+  }
+</style>
